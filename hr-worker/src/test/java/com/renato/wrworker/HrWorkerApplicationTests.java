73d9b977c0acbf1d7285825712b7d296580bb66a
Jrenatomt <jrenatomt@gmail.com>
2021-01-18 21:10:19 -0300

    Project created

---
+++ hr-worker/src/test/java/com/renato/wrworker/HrWorkerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.renato.wrworker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HrWorkerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
