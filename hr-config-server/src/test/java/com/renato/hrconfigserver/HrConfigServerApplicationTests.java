b5a2c9a62b8c4886d7cd607456ea16748332ee97
Jrenatomt <jrenatomt@gmail.com>
2021-01-26 21:45:24 -0300

    Config server created

---
+++ hr-config-server/src/test/java/com/renato/hrconfigserver/HrConfigServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.renato.hrconfigserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HrConfigServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
